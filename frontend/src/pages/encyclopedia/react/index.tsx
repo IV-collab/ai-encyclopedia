59614bc0693e3c0345eb8a532198b6f7a4bf8656
Andy Jung <wontaethedev@gmail.com>
2024-09-06 17:56:18 -0400

    Add shadcn and test accordion component

--- frontend/src/pages/encyclopedia/react/index.tsx
+++ frontend/src/pages/encyclopedia/react/index.tsx
@@ -1,6 +1,20 @@
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion"
+
 const ReactPage = () => {
     return (
-      <div>React</div>
+      <Accordion type="single" collapsible>
+        <AccordionItem value="item-1">
+          <AccordionTrigger>Accordion trigger</AccordionTrigger>
+          <AccordionContent>
+            Accordion contents
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
     );
   };
   
