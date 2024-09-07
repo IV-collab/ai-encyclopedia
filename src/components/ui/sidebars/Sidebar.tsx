e4f1908cbd27a8cecfc53fd3abe6056e2382ff3e
Andy Jung <wontaethedev@gmail.com>
2024-09-06 22:58:30 -0400

    Create reusable icon, add capybara icon, add icon to sidebar

--- src/components/ui/sidebars/Sidebar.tsx
+++ src/components/ui/sidebars/Sidebar.tsx
@@ -1,5 +1,9 @@
 import { SidebarItemsTypes } from '@/lib/enums/sidebar';
+import { IconTypes } from '@/lib/enums/icon';
+import { BRAND_TITLE } from '@/lib/enums/generic';
+
 import { IVSidebarItems } from '@/components/ui/sidebars/SidebarItems';
+import { IVIcon } from '@/components/ui/icons/Icon';
 
 export type SidebarItem = {
   type: SidebarItemsTypes;
@@ -10,7 +14,12 @@ export type SidebarItem = {
 export function IVSidebar({ items }: { items: SidebarItem[] }) {
   return (
     <div className="iv-sidebar w-96 min-h-screen pl-8 pr-8 pt-8 bg-secondary">
-      <div className="text-primary pb-10 text-2xl border-b font-semibold">AI Encyclopedia</div>
+      <div className="flex flex-row items-center pb-10 border-b">
+        <IVIcon type={IconTypes.CAPYBARA} />
+        <div className="flex items-center ml-2">
+          <p className="text-primary text-3xl font-medium">{BRAND_TITLE}</p>
+        </div>
+      </div>
       <IVSidebarItems items={items} />
     </div>
   );
