6524726997cf106b1171822ec42340165eda2339
Andy Jung <wontaethedev@gmail.com>
2024-09-07 11:58:40 +0900

    Icon (#13)
    * Add capybara icon
    
    * Create reusable icon, add capybara icon, add icon to sidebar

---
+++ src/components/ui/icons/Icon.tsx
@@ -0,0 +1,23 @@
+import capybaraHead from '@/assets/capybara_head.svg';
+
+import { IconTypes, IconSizes } from '@/lib/enums/icon';
+
+export function IVIcon({ type, size = IconSizes.MEDIUM }: { type: IconTypes; size?: IconSizes }) {
+  /**
+   * Returns the src of an icon depending on the type given.
+   * @param type The type of icon to use. Dictates which image is displayed.
+   * @returns The src of the icon to display
+   */
+  const iconSrc = (type: IconTypes): string => {
+    switch (type) {
+      case IconTypes.CAPYBARA:
+        return capybaraHead;
+      default:
+        return capybaraHead;
+    }
+  };
+
+  return <img src={iconSrc(type)} alt="Logo" className={size} />;
+}
+
+export default IVIcon;
---
+++ src/lib/enums/icon.ts
@@ -0,0 +1,10 @@
+export enum IconTypes {
+  CAPYBARA = 'capybara',
+}
+
+export enum IconSizes {
+  SMALL = 'h-8 w-8',
+  MEDIUM = 'h-10 w-10',
+  LARGE = 'h-24 w-24',
+  XLARGE = 'h-36 w-36',
+}
