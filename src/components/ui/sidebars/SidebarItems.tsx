23095fbc12a8391460135dcde9fbaa21d0e549b8
Andy Jung <wontaethedev@gmail.com>
2024-09-06 22:46:48 -0400

    Refactor complex HTML code into readable, separate logics and functions

--- src/components/ui/sidebars/SidebarItems.tsx
+++ src/components/ui/sidebars/SidebarItems.tsx
@@ -7,33 +7,42 @@ export function IVSidebarItems({ items, level = 0 }: { items: SidebarItem[]; lev
   // When level is 0 (The first level of items), show bottom border under each item
   const enableBorder: boolean = level == 0;
 
+  const recursiveAccordionItem = (index: number, trigger: string, content: SidebarItem[] | string) => {
+    if (typeof content == 'string') {
+      return 'ERROR';
+    } else {
+      return (
+        // Value of item must be unique, such that triggers don't interfere with other items
+        <AccordionItem value={`item-${index}`} enableBorder={enableBorder}>
+          <AccordionTrigger>{trigger}</AccordionTrigger>
+          <AccordionContent>
+            <IVSidebarItems items={content} level={level + 1} />
+          </AccordionContent>
+        </AccordionItem>
+      );
+    }
+  };
+
+  const defaultAccordionItem = (index: number, trigger: string, content: SidebarItem[] | string) => {
+    return (
+      // Value of item must be unique, such that triggers don't interfere with other items
+      <AccordionItem value={`item-${index}`} enableBorder={enableBorder}>
+        <AccordionTrigger>{trigger}</AccordionTrigger>
+        <AccordionContent>{typeof content == 'string' ? content : 'ERROR'}</AccordionContent>
+      </AccordionItem>
+    );
+  };
+
   return (
     <Accordion type="single" collapsible>
       {items && items.length > 0 ? (
         items.map((item, index) =>
-          item.type == SidebarItemsTypes.NESTED ? (
-            // If item is nested, then recursively render item
-            typeof item.content == 'string' ? (
-              'ERROR'
-            ) : (
-              // Value of item must be unique, such that triggers don't interfere with other items
-              <AccordionItem value={`item-${index}`} enableBorder={enableBorder}>
-                <AccordionTrigger>{item.trigger}</AccordionTrigger>
-                <AccordionContent>
-                  <IVSidebarItems items={item.content} level={level + 1} />
-                </AccordionContent>
-              </AccordionItem>
-            )
-          ) : (
-            // If item is not of a special type, render item directly
-            <AccordionItem value={`item-${index}`} enableBorder={enableBorder}>
-              <AccordionTrigger>{item.trigger}</AccordionTrigger>
-              <AccordionContent>{typeof item.content == 'string' ? item.content : 'ERROR'}</AccordionContent>
-            </AccordionItem>
-          ),
+          item.type == SidebarItemsTypes.NESTED
+            ? recursiveAccordionItem(index, item.trigger, item.content)
+            : defaultAccordionItem(index, item.trigger, item.content),
         )
       ) : (
-        <div></div>
+        <div>ERROR</div>
       )}
     </Accordion>
   );
