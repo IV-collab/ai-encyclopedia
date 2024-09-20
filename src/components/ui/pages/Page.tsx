b78f72fcc9442edf5df2c2d8f77873ed7336dc8f
Andy Jung <wontaethedev@gmail.com>
2024-09-20 13:37:43 +0900

    Make sidebar collapsible, implement provider and context files for sharing sidebar collapse state, make common page component react to sidebar collapse

--- src/components/ui/pages/Page.tsx
+++ src/components/ui/pages/Page.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 
+import { useSidebarContext } from '@/contexts';
+
 export function IVPage({ title, content }: { title: string; content?: React.ReactNode }) {
+  const { isCollapsed } = useSidebarContext();
+
   return (
-    <div className="iv-page size-full m-0 pt-40 pl-60 pr-60 flex flex-col text-nearBlack">
-      <div className="title-wrapper text-4xl font-bold mb-10">
+    <div
+      className={`iv-page ${isCollapsed ? 'w-[50%]' : 'w-[70%]'} m-0 pt-40 flex flex-col items-center text-nearBlack`}
+    >
+      <div className="title-wrapper w-[100%] text-4xl font-bold mb-10">
         <p>{title}</p>
       </div>
       <div className="content">{content}</div>
--- src/components/ui/sidebars/Sidebar.tsx
+++ src/components/ui/sidebars/Sidebar.tsx
@@ -1,6 +1,10 @@
+import { Link } from 'react-router-dom';
+
 import { SidebarItemTypes } from '@/lib/enums/sidebar';
 import { IconTypes } from '@/lib/enums/icon';
 import { BRAND_TITLE } from '@/lib/enums/generic';
+
+import { useSidebarContext } from '@/contexts';
 
 import { IVSidebarItems } from '@/components/ui/sidebars/SidebarItems';
 import { IVIcon } from '@/components/ui/icons/Icon';
@@ -14,17 +18,59 @@ export type SidebarItem = {
 };
 
 export function IVSidebar({ items }: { items: SidebarItem[] }) {
-  return (
-    <div className="iv-sidebar w-96 min-h-screen pl-8 pr-8 pt-8 bg-secondary">
-      <div className="flex flex-row items-center pb-10 border-b">
-        <IVIcon type={IconTypes.CAPYBARA} />
-        <div className="flex items-center ml-2">
-          <p className="text-primary text-3xl font-medium">{BRAND_TITLE}</p>
+  const { isCollapsed, toggleCollapse } = useSidebarContext();
+
+  const collapsedSidebar = (): React.ReactNode => {
+    return (
+      <div className="w-10 h-screen pt-4 pl-1 pr-1">
+        <div
+          className="collapsible-wrapper p-1 cursor-pointer flex justify-center items-center"
+          onClick={toggleCollapse}
+        >
+          <p className="text-xs">{'>>'}</p>
         </div>
       </div>
-      <IVSidebarItems items={items} />
-    </div>
-  );
+    );
+  };
+
+  const expandedSidebar = (): React.ReactNode => {
+    return (
+      <div className="group min-w-96 min-h-screen bg-secondary flex flex-col justify-between pl-7 pr-7">
+        <div>
+          {/* COLLAPSIBLE */}
+          <div
+            className="flex justify-end items-center pt-4 pb-3 cursor-pointer opacity-0 group-hover:opacity-100"
+            onClick={toggleCollapse}
+          >
+            <div className="p-1">
+              <p className="text-xs">{'<<'}</p>
+            </div>
+          </div>
+          {/* BRAND */}
+          <div className="flex flex-row items-center pb-10 border-b">
+            <div className="brand-container flex flex-row">
+              <IVIcon type={IconTypes.CAPYBARA} />
+              <div className="flex items-center ml-2">
+                <p className="text-primary text-3xl font-medium">{BRAND_TITLE}</p>
+              </div>
+            </div>
+          </div>
+          {/* ITEMS LIST */}
+          <div>
+            <IVSidebarItems items={items} />
+          </div>
+        </div>
+        {/* Footer */}
+        <div className="pb-10 pt-10 border-t">
+          <div className="font-medium">
+            <Link to={'slate'}>Generate content</Link>
+          </div>
+        </div>
+      </div>
+    );
+  };
+
+  return <div className="iv-sidebar">{isCollapsed ? collapsedSidebar() : expandedSidebar()}</div>;
 }
 
 export default IVSidebar;
